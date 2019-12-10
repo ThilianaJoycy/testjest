7f2e7bbd2fc1d2466cdb4ff1cee50c493d448cb1
joyce <joyce.yimga@sprint-pay.com>
2019-12-10 14:43:30 +0100

    commit

--- src/jest.config.js
+++ src/jest.config.js
@@ -3,5 +3,6 @@ const {defaults} = require('jest-config');
 module.exports = {
   verbose: true,
   collectCoverage: true,
-  collectCoverageFrom: ["src/**/*.ts"]
+  collectCoverageFrom: ["src/**/*.ts"],
+  testResultsProcessor: "jest-jenkins-reporter"
 }
