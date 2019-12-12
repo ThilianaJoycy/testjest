8e1c6ea1b41259566f7d478a0dd35328efabe77b
joyce <joyce.yimga@sprint-pay.com>
2019-12-12 17:39:00 +0100

    commit

--- src/jest.config.js
+++ src/jest.config.js
@@ -1,6 +1,21 @@
 const {defaults} = require('jest-config');
 
 module.exports = {
+  preset: 'jest-preset-angular',
+  roots: ['src'],
+  transform: {
+    '^.+\\.(ts|js|html)$': 'ts-jest',
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
+  moduleNameMapper: {
+    '@app/(.*)': '<rootDir>/src/app/$1',
+    '@assets/(.*)': '<rootDir>/src/assets/$1',
+    '@core/(.*)': '<rootDir>/src/app/core/$1',
+    '@env': '<rootDir>/src/environments/environment',
+    '@src/(.*)': '<rootDir>/src/src/$1',
+    '@state/(.*)': '<rootDir>/src/app/state/$1'
+  },
+  transformIgnorePatterns: ['node_modules/(?!@babel/)'],
   verbose: true,
   collectCoverage: true,
   collectCoverageFrom: ["src/**/*.ts"],
@@ -9,9 +24,7 @@ module.exports = {
     "cobertura"
   ],
   moduleDirectories: ["node_modules", "src"],
-  transform: {
-    '^.+\\.(ts|js|html)$': 'ts-jest',
-  },
-  transformIgnorePatterns: ['node_modules/(?!@babel/)'],
+  
+  
   testResultsProcessor: "jest-jenkins-reporter"
 }
