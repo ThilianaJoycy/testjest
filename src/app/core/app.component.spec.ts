e03eaa6a356d179815852b4837dd2b2994ed9af6
joyce <joyce.yimga@sprint-pay.com>
2019-12-13 09:17:06 +0100

    commit

--- src/app/core/app.component.spec.ts
+++ src/app/core/app.component.spec.ts
@@ -1,3 +1,6 @@
+import 'core-js/es7/reflect';
+import 'zone.js/dist/zone';
+import 'zone.js/dist/zone-testing';
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
